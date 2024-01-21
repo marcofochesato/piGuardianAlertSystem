6ce3fda0bc5ab9619ebcc2a5493c241f5fda9e28
marcofoc <marcofoc@gmail.com>
2024-01-21 10:41:41 +0100

    Add alert sending functionality to TelegramSender
    A new function 'send_alert' has been added in 'telegram_sender.py', enabling the bot to send alerts to chat IDs. The method loops over a list of chat IDs, attempting to send out a message to each one, providing feedback if successful, and error messages if there are issues sending alerts.

--- telegram_sender.py
+++ telegram_sender.py
@@ -7,6 +7,14 @@ class TelegramService:
         self.bot = telepot.Bot(bot_token)
         self.chat_ids = chat_ids
 
+    def send_alert(self, message):
+        for chat_id in self.chat_ids:
+            try:
+                self.bot.sendMessage(chat_id, message, parse_mode='Markdown')
+                print(f"Telegram alert sent to chat ID {chat_id}")
+            except Exception as e:
+                print(f"Error sending Telegram alert to chat ID {chat_id}: {e}")
+
 
 def start_telegram_service(bot_token, chat_ids):
     telegram_service = TelegramService(bot_token, chat_ids)
